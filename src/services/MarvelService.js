2d25a3d411bc81793d0efe791050a0d6fc99b63d
Sever0k <sever0k@ukr.net>
2022-01-31 22:14:56 +0300

    API_(get_zapros_marvel_api)

---
+++ src/services/MarvelService.js
@@ -0,0 +1,27 @@
+
+
+class MarvelService {
+
+_apiBase='https://gateway.marvel.com:443/v1/public/';
+_apiKey = 'apikey=28091c665142cf6232bd533020c59f12';
+
+    getResoure = async (url) => {
+        let res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResoure(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResoure(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+
+}
+
+export default MarvelService;
